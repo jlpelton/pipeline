3b2f3ee8736fb4358130e08e27b4314a100526b7
jlpelton <jennifer.pelton@hmhco.com>
2015-09-15 17:23:46 +0000

    changed typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Pipeline::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  cconfig.assets.compile = true
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
